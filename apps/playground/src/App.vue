22cf771ec4e0f8acf9372d9d4c827ac427b84873
cxs <cxs@wepig.cn>
2025-04-22 09:36:51 +0800

    fix:fix some bug

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -6,9 +6,9 @@
 </template>
 <script lang="ts" setup>
   //直接引用packages组件
-  // import { MyButton } from '@my-lib/components'
+  import { MyButton } from '@my-lib/components'
   //引用打包后的lib组件
-  import { MyButton } from 'my-lib'
+  // import { MyButton } from 'my-lib'
   import { useDarkMode } from './hooks/ToggleDarkMode'
   const mode = useDarkMode()
   console.log(mode)
--- apps/playground/tailwind.config.js
+++ apps/playground/tailwind.config.js
@@ -1,5 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-const sharedConfig = require('tailwind-config/tailwind.config.js')
 module.exports = {
-  presets: [sharedConfig],
+  content: [
+    `./index.html`,
+    `./src/**/*.{vue,js,ts,jsx,tsx}`,
+    `./node_modules/@wepig-app/components/**/*.{vue,js,ts,jsx,tsx}`,
+    `../../packages/components/**/*.{vue,js,ts,jsx,tsx}`,
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+  },
 }
